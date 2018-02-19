d4471fa5e658cc151c3573a104e10c6e422965c5
drincastX <drincast@gmail.com>
2018-02-19 17:04:58 -0500

    se realiza conexión a mongo lab

--- apiinterlibros/index.js
+++ apiinterlibros/index.js
@@ -9,11 +9,11 @@ var dato = require("./src/dato/dato");
 const basedatos = process.env.MONGO_URL || "mongodb://interlibros:interlibros123@ds041678.mlab.com:41678/interlibros";
 
 mongoose.connect(basedatos, function(err, res) {
-  console.log('iniciando conexión a mongo lab ...');
+  console.log('iniciando conexión a mongolab ...');
 
   if(err){
     console.log('Error de conexión: ' + err);
   }
 
-  console.log('Connected to Database');
+  console.log('Se conecto a mongolab !!!!'); 
 });
